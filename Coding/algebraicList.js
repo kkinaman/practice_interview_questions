1f1ecfabfa7fd692967b5aa84d8ad65a31e07e58
Karina Kinaman <kkinaman419@gmail.com>
2016-12-28 10:40:59 -0500

    New problem: algebraicList

---
+++ Coding/algebraicList.js
@@ -0,0 +1,31 @@
+function Cons(head, tail) {
+  this.head = head;
+  this.tail = tail;
+}
+
+Cons.fromArray = function(array) {
+  if (!array.length) {
+    return null;
+  } else {
+    return new Cons(array[0], Cons.fromArray(array.slice(1)));
+  }
+};
+
+function filter(list, predicate) {
+  let results = [];
+  if (predicate(list.head)) {
+    results.push(list.head);
+  }
+  //TODO: return a new list containing only elements
+  //that satisfy the predicate function.
+  return results;
+}
+
+function map(list, mapper){
+  //TODO: return a new list containing all elements
+  //resulting from applying the mapper functiont to them
+  return null;
+}
+
+Cons.prototype.filter = function(predicate){ return filter(this,predicate); };
+Cons.prototype.map = function(mapper){ return map(this, mapper); };
