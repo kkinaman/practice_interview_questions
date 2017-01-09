17d470467615fd6c91a2161536d97a9a7a84466a
Karina Kinaman <kkinaman419@gmail.com>
2017-01-08 22:35:23 -0800

    Implement routingCheck

--- Coding/routingCheck.js
+++ Coding/routingCheck.js
@@ -1,2 +1,11 @@
 //write a function that checks if a given string is a valid routing number
 
+function isValid(str) {
+  if (str.length !== 9) {
+    return false;
+  }
+  let num = str.split('').map(digit => parseInt(digit));
+  return (3 * (num[0] + num[3] + num[6]) + 7 * (num[1] + num[4] + num[7]) + (num[2] + num[5] + num[8])) % 10 === 0;
+}
+
+console.log(isValid('011400071'));
