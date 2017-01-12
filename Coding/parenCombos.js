49d625bed71eef61972bbddbe264d4d9c00e18de
Karina Kinaman <kkinaman419@gmail.com>
2017-01-12 11:47:45 -0800

    Add new prompt: parenCombos

---
+++ Coding/parenCombos.js
@@ -0,0 +1,14 @@
+/*
+Given an integer n, which represents a number of paren pairs, output all combinations of n valid parens
+
+Example:
+  n = 3
+  output: ['()()()', '()(())', '(())()', '(()())', ((()))']
+*/
+
+function parenCombos(n) {
+  return [];
+}
+
+var n = 3;  //['()()()', '()(())', '(())()', '(()())', ((()))']
+console.log(parenCombos(n));
