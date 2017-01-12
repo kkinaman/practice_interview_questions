7c4136ab5b376327c918d55430d03540e990aa4f
Karina Kinaman <kkinaman419@gmail.com>
2017-01-12 12:03:52 -0800

    Implement parenCombos

--- Coding/parenCombos.js
+++ Coding/parenCombos.js
@@ -8,19 +8,35 @@ Example:
 
 function parenCombos(n) {
   //define array of possibilities
+  let possibilities = [];
   //define recursive helper function with inputs stringSoFar, numToOpen, numToClose
-    //if numtoClose is 0 and numToOpen > 0
+  function recurse(stringSoFar, numToOpen, numToClose) {
+    //if there are no more to close and still some to open
+    if (numToClose === 0 && numToOpen > 0) {
       //only option is to open -- recurse on string plus '(' with numToOpen - 1, numToClose = 1
-    //if numToOpen is 0
-      //only option is to close the rest -- while numToClose > 0
-        // add ')'
+      recurse(stringSoFar + '(', numToOpen - 1, 1);
+    } else if (numToOpen === 0) { //if there are no more to open
+      //only option is to close the rest -- while numToClose > 0, add ')'
+      while (numToClose > 0) {
+        stringSoFar += ')';
+        numToClose--;
+      }
       //add this option to possibilities
-    //else
+      possibilities.push(stringSoFar);
+    } else {
       //recurse on string plus a new '(' with numToOpen - 1, numToClose + 1
+      recurse(stringSoFar + '(', numToOpen - 1, numToClose + 1);
       //recurse on string plus a new ')' with numToOpen the same, numToClose - 1
-  //return possibilites
-  return [];
+      recurse(stringSoFar + ')', numToOpen, numToClose - 1);
+    }
+  }
+  //call recurse on empty string with n to open, 0 to close
+  recurse('', n, 0);
+  //return possibilities
+  return possibilities;
 }
 
 var n = 3;  //['()()()', '()(())', '(())()', '(()())', ((()))']
+// var n = 2;
+// var n = 4;
 console.log(parenCombos(n));
