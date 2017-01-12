72a29392a1eabc7a5c3cad36e5fad8ed7967a30a
Karina Kinaman <kkinaman419@gmail.com>
2017-01-12 11:33:03 -0800

    Add new prompt: moveZeros

---
+++ Coding/moveZeros.js
@@ -0,0 +1,14 @@
+/*
+Given an array of integers, output an array with all 0s shifted to the left, maintaining the order of all other elements
+Example: 
+  input: [2, 6, 14, 0, 3, 0, 10]
+  output: [0, 0, 2, 6, 14, 3, 10]
+Constraints: O(n) time, O(1) space
+*/
+
+function moveZeros(array) {
+  return [];
+}
+
+var input = [2, 6, 14, 0, 3, 0, 10];
+console.log(moveZeros(input)); //[0, 0, 2, 6, 14, 3, 10]
