066633fc0552dc6713d37708f9eaa6999f7d8680
Karina Kinaman <kkinaman419@gmail.com>
2017-01-12 11:25:15 -0800

    Implement stockPrices in O(n) time, O(1) space

--- Coding/stockPrices.js
+++ Coding/stockPrices.js
@@ -1,8 +1,36 @@
-function getMaxProit(stockPrices) {
-
+function getMaxProfit(stockPrices) {
+  //start min pointer at 0
+  let minP = 0;
+  //start max pointer at 1
+  let maxP = 1;
+  //save current min and max
+  let curMin = stockPrices[minP];
+  let curMax = stockPrices[maxP];
+  //save largest difference as max - min
+  let largestProfit = curMax - curMin;
+  //while before the end
+  while (maxP < stockPrices.length) {
+    //move both pointers up by one
+    minP++;
+    maxP++;
+    //if the difference between the new max and new min is greater than current profit
+    if (stockPrices[maxP] - stockPrices[minP] > largestProfit) {
+      curMax = stockPrices[maxP];
+      //ensure that keeping min where it is wouldn't be a bigger profit before updating min
+      if (stockPrices[maxP] - curMin < stockPrices[maxP] - stockPrices[minP]) {
+        curMin = stockPrices[minP];
+      }
+      largestProfit = curMax - curMin;
+    } else if (stockPrices[maxP] - curMin > largestProfit) {
+      curMax = stockPrices[maxP];
+      largestProfit = curMax - curMin;
+    }
+  }
+  return largestProfit;
 }
 
-var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+var stockPricesYesterday = [10, 7, 5, 8, 6, 11, 9]; // 6
+// var stockPricesYesterday = [10, 8, 5, 4, 1, 0]; // -1
 
-getMaxProfit(stockPricesYesterday);
+console.log(getMaxProfit(stockPricesYesterday));
 // returns 6 (buying for $5 and selling for $11)
