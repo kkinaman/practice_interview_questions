d4c1ff8eccff177290c1981bd5877a4571a1f8ec
Karina Kinaman <kkinaman419@gmail.com>
2017-01-12 11:53:58 -0800

    Psuedocode

--- Coding/parenCombos.js
+++ Coding/parenCombos.js
@@ -7,6 +7,18 @@ Example:
 */
 
 function parenCombos(n) {
+  //define array of possibilities
+  //define recursive helper function with inputs stringSoFar, numToOpen, numToClose
+    //if numtoClose is 0 and numToOpen > 0
+      //only option is to open -- recurse on string plus '(' with numToOpen - 1, numToClose = 1
+    //if numToOpen is 0
+      //only option is to close the rest -- while numToClose > 0
+        // add ')'
+      //add this option to possibilities
+    //else
+      //recurse on string plus a new '(' with numToOpen - 1, numToClose + 1
+      //recurse on string plus a new ')' with numToOpen the same, numToClose - 1
+  //return possibilites
   return [];
 }
 
