1b7c2c615443ee8802485d07faadcbff18d05d7b
Karina Kinaman <kkinaman419@gmail.com>
2017-01-17 20:09:43 -0500

    Add alternative solution to largestProductAlt
    Keep track of highest positive and lowest negative numbers instead of highest and lowest products of two

---
+++ Coding/largestProductAlt.js
@@ -0,0 +1,7 @@
+/*
+Given an arrayOfInts, find the largestProductOfThree you can get from three of the integers.
+Constraints: linear time, constant space
+*/
+
+function largestProductOfThree(arrayOfInts) {
+}
