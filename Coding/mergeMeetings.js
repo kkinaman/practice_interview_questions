0a8ec57552630a6f41c933e019c4798b333bf3dc
Karina Kinaman <kkinaman419@gmail.com>
2017-01-17 19:37:59 -0500

    Add new problem: mergeMeetings

---
+++ Coding/mergeMeetings.js
@@ -0,0 +1,15 @@
+/*
+Given an array of meeting times of employees(object formatted as below), 
+output a condensed array with all times anyone is in a meeting.
+
+input: [
+        {startTime: 2, endTime: 4},
+        {startTime: 6, endTime: 10}]
+
+Note: The integer values for startTime and endTime are the number of 30-min intervals after 9:00AM
+
+*/
+
+function mergeMeetings(meetingTimes) {
+  
+}
