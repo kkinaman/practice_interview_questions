d97213800d3ec3e46bb2d823905c08227d1c1e17
Karina Kinaman <kkinaman419@gmail.com>
2017-01-17 19:32:56 -0500

    Handle more cases of pos/neg combinations

--- Coding/largestProduct.js
+++ Coding/largestProduct.js
@@ -8,46 +8,56 @@ function largestProductOfThree(arrayOfInts) {
 
   //declare variables to keep track of the 'highest' integers so far
   let highestPosProd = 0; //highestPosProd is the product of the two highest positive integers
-  let mostPositive = -Infinity; //the highest positive integer
+  let mostPositive = null; //the highest positive integer
   let lowestNegProd = 0; //lowestNegProd is the product of the most positive positive integer and most negative integer
-  let mostNegative = Infinity; //the lowest negative integer
+  let mostNegative = null; //the lowest negative integer
 
   //consider the first two integers and save them to the appropriate variables
   if (arrayOfInts[0] * arrayOfInts[1] > highestPosProd) { //if both are positive or both are negative
     highestPosProd = arrayOfInts[0] * arrayOfInts[1];
-    mostPositive = Math.max(arrayOfInts[0], arrayOfInts[1]);
+    mostPositive = Math.max(arrayOfInts[0], arrayOfInts[1]) > 0 ? Math.max(arrayOfInts[0], arrayOfInts[1]) : null;
+    mostNegative = Math.min(arrayOfInts[0], arrayOfInts[1]) < 0 ? Math.min(arrayOfInts[0], arrayOfInts[1]) : null;
   } else if (arrayOfInts[0] * arrayOfInts[1] < lowestNegProd) { //if one is pos and one is neg
     lowestNegProd = arrayOfInts[0] * arrayOfInts[1];
+    mostPositive = Math.max(arrayOfInts[0], arrayOfInts[1]);
     mostNegative = Math.min(arrayOfInts[0], arrayOfInts[1]);
   }
+
+  //** At this point, we know either highestPosProd exists, or we have at least one positive and one negative number
+
   //TODO WHAT ABOUT ZERO
   //at each new element, check if it times the larger two would be larger than the largest product
   for (let i = 2; i < arrayOfInts.length; i++) {
     let cur = arrayOfInts[i];
-    if (highestPosProd * cur > largestProduct) { // then cur must also be positive
+    if (highestPosProd && highestPosProd * cur > largestProduct) {
       largestProduct = highestPosProd * cur;
-      if (cur > (highestPosProd / mostPositive)) { //if cur is greater than the lower of the highest two positive integers
+      if (mostPositive && cur > (highestPosProd / mostPositive)) { //if highestPosProd is the product of positive integers and cur is greater than the lower of these numbers
         highestPosProd = cur * mostPositive; //update highestPosProd
         mostPositive = Math.max(cur, mostPositive); //update mostPositive if cur is also greater than current mostPositive
         lowestNegProd = mostPositive * mostNegative; //update lowestNegProd incase mostPositive was updated
+      } else {
+        mostPositive = cur > 0 ? cur : null;
       }
-    } else if (lowestNegProd * cur > largestProduct) { //then cur must be negative
+    } else if (lowestNegProd && lowestNegProd * cur > largestProduct) { //then cur must be negative
       largestProduct = lowestNegProd * cur;
       mostNegative = Math.min(cur, mostNegative); //update mostNegative if cur is less than current mostNegative
       lowestNegProd = mostPositive * mostNegative; //update lowestNegProd incase mostNegative was updated
+    } else if (mostNegative * cur > highestPosProd) { //this is the case if the first two were one pos, one neg, and the third is negative
+      highestPosProd = mostNegative * cur;
+      mostNegative = Math.min(mostNegative, cur);
     }
   }
   return largestProduct;
 }
 
 //all positives
-console.log(largestProductOfThree([4, 2, 8, 1, 3]));    // 96 TODO
+console.log(largestProductOfThree([4, 2, 8, 1, 3]));    // 96
 console.log(largestProductOfThree([1, 2, 3, 4, 5]));    // 60
 //all negatives
-console.log(largestProductOfThree([-3, -10, -1, -2]));  // -6
-console.log(largestProductOfThree([-3, -2, -1]));  // -6
+console.log(largestProductOfThree([-3, -10, -1, -2]));  // -6 TODO
+console.log(largestProductOfThree([-3, -2, -1]));       // -6
 //mix of positive and negatives
-console.log(largestProductOfThree([2, 5, -6, 7, 1]));   // 70 TODO
+console.log(largestProductOfThree([2, 5, -6, 7, 1]));   // 70
 console.log(largestProductOfThree([2, -5, -6, 7, 1]));  // 210 TODO
 console.log(largestProductOfThree([-1, 2, -3, 4]));     // 12 TODO
 //include 0
