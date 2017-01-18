e13c38b49d25775f1af34762436882f600cf8600
Karina Kinaman <kkinaman419@gmail.com>
2017-01-17 20:45:22 -0500

    Handle arrays with all negative integers

--- Coding/largestProductAlt.js
+++ Coding/largestProductAlt.js
@@ -13,6 +13,7 @@ function largestProductOfThree(arrayOfInts) {
   let largestProduct = -Infinity;
   let highestPositives = [];
   let lowestNegatives = [];
+  let highestNegatives = []; //in the case of all negative integers, we'll want the 'smallest' (highest-valued) negatives
   let hasZero = false;
 
   for (let i = 0; i < 2; i++) {
@@ -21,6 +22,7 @@ function largestProductOfThree(arrayOfInts) {
       highestPositives.push(cur);
     } else if (cur < 0) {
       lowestNegatives.push(cur);
+      highestNegatives.push(cur);
     } else {
       hasZero = true;
     }
@@ -28,12 +30,18 @@ function largestProductOfThree(arrayOfInts) {
 
   for (let i = 2; i < arrayOfInts.length; i++) {
     let cur = arrayOfInts[i];
+    //multiplying with the highest positives (cur must also be positive)
     if (highestPositives.length === 2 && cur * highestPositives[0] * highestPositives[1] > largestProduct) {
       largestProduct = cur * highestPositives[0] * highestPositives[1];
+    //multiplying with the lowest negatives (cur must be positive)
     } else if (lowestNegatives.length === 2 && cur * lowestNegatives[0] * lowestNegatives[1] > largestProduct) {
       largestProduct = cur * lowestNegatives[0] * lowestNegatives[1];
+    //mutiplying with the highest positive and lowest negavtive (cur is either negative, or largestProduct is negative)
     } else if (highestPositives.length && lowestNegatives.length && cur * findLargest(highestPositives) * findSmallest(lowestNegatives) > largestProduct) {
       largestProduct = cur * findLargest(highestPositives) * findSmallest(lowestNegatives);
+    //multiplying with the highest-value negatives (cur is also negative --> largestProduct is also negative)
+    } else if (highestNegatives.length === 2 && cur * highestNegatives[0] * highestNegatives[1] > largestProduct) {
+      largestProduct = cur * highestNegatives[0] * highestNegatives[1];
     }
 
     if (cur > 0) {
@@ -48,6 +56,11 @@ function largestProductOfThree(arrayOfInts) {
       } else if (cur < Math.max(lowestNegatives[0], lowestNegatives[1])) {
         lowestNegatives = [cur, Math.min(lowestNegatives[0], lowestNegatives[1])];
       }
+      if (highestNegatives.length < 2) {
+        highestNegatives.push(cur);
+      } else if (cur > Math.min(highestNegatives[0], highestNegatives[1])) {
+        highestNegatives = [cur, Math.max(highestNegatives[0], highestNegatives[1])];
+      }
     } else {
       hasZero = true;
     }
@@ -59,7 +72,7 @@ function largestProductOfThree(arrayOfInts) {
 console.log(largestProductOfThree([4, 2, 8, 1, 3]));    // 96
 console.log(largestProductOfThree([1, 2, 3, 4, 5]));    // 60
 //all negatives
-console.log(largestProductOfThree([-3, -10, -1, -2]));  // -6 TODO
+console.log(largestProductOfThree([-3, -10, -1, -2]));  // -6
 console.log(largestProductOfThree([-3, -2, -1]));       // -6
 //mix of positive and negatives
 console.log(largestProductOfThree([2, 5, -6, 7, 1]));   // 70
