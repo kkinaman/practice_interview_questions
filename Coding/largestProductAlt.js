6579923b4813190e8152f3ad1c09d4d4754e66a2
Karina Kinaman <kkinaman419@gmail.com>
2017-01-17 20:50:45 -0500

    Add test cases and assumptions

--- Coding/largestProductAlt.js
+++ Coding/largestProductAlt.js
@@ -3,6 +3,8 @@ SOLUTION INVOLVING KEEPING TRACK OF TWO HIGHEST AND TWO LOWEST INTEGERS
 
 Given an arrayOfInts, find the largestProductOfThree you can get from three of the integers.
 Constraints: linear time, constant space
+
+Note: Assuming valid input of an array of at least 3 integers
 */
 
 function largestProductOfThree(arrayOfInts) {
@@ -76,7 +78,11 @@ console.log(largestProductOfThree([-3, -10, -1, -2]));  // -6
 console.log(largestProductOfThree([-3, -2, -1]));       // -6
 //mix of positive and negatives
 console.log(largestProductOfThree([2, 5, -6, 7, 1]));   // 70
-console.log(largestProductOfThree([2, -5, -6, 7, 1]));  // 210
+console.log(largestProductOfThree([2, 5, 1, -4, 3]));   // 30
+console.log(largestProductOfThree([-1, -4, 1, 4]));     // 16
+console.log(largestProductOfThree([-1, -4, -2, 3]));    // 24
+console.log(largestProductOfThree([2, -5, -6, 7, -1])); // 210
 console.log(largestProductOfThree([-1, 2, -3, 4]));     // 12
 //include 0
 console.log(largestProductOfThree([4, 0, -5, 2]));      // 0
+console.log(largestProductOfThree([0, 0, 0]));          // 0
