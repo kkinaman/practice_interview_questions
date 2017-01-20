39a806cc53335776ebe5c75ac425e86a9535522a
Karina Kinaman <kkinaman419@gmail.com>
2017-01-20 09:05:44 -0800

    Add new problem sortAnagrams

---
+++ Coding/sortAnagrams.js
@@ -0,0 +1,11 @@
+/*
+Given an array of words, output together words that are anagrams of each other, separating groups of words by a new line
+
+e.g.
+
+Input: [act, cat, spot, stop, go]
+Output: 
+act cat
+spot stop
+go
+/*
