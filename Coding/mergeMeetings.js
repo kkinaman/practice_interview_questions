65e126732111f6c866a875f0339b5eeb5836d2dd
Karina Kinaman <kkinaman419@gmail.com>
2017-01-20 09:32:59 -0800

    Solve in O(nlogn) time

--- Coding/mergeMeetings.js
+++ Coding/mergeMeetings.js
@@ -21,29 +21,52 @@ Example:
 Constraints: can be done in O(nlogn) time
 */
 
-//TODO: Optimize based on interviewcake suggestions
+//TODO: Optimize based on interviewcake suggestions -- currently some factor of n^2...
 
-function mergeMeetings(meetingTimes) {
-  let busyTimes = {};
-  meetingTimes.forEach(meeting => {
-    //mark every 30 min slot if it is busy
-    for (let i = meeting.startTime; i < meeting.endTime; i++) {
-      busyTimes[i] ? null : busyTimes[i] = i;
-    }
+// function mergeMeetings(meetingTimes) {
+//   let busyTimes = {};
+//   meetingTimes.forEach(meeting => {
+//     //mark every 30 min slot if it is busy
+//     for (let i = meeting.startTime; i < meeting.endTime; i++) {
+//       busyTimes[i] ? null : busyTimes[i] = i;
+//     }
+//   });
+//   let times = Object.keys(busyTimes).map(Number);
+//   let iter = 0;
+//   let mergedTimes = [];
+//   while (iter < times.length) {
+//     let newMeeting = { startTime: times[iter], endTime: null};
+//     iter++;
+//     while (times[iter] - times[iter - 1] === 1) {
+//       iter++;
+//     }
+//     newMeeting.endTime = times[iter - 1] + 1;
+//     mergedTimes.push(newMeeting);
+//   }
+//   return mergedTimes;
+// }
+
+function mergeMeetings(meetings) {
+  let busyTimes = [];
+  //sort meetings by start time O(nlgn)
+  meetings.sort((a, b) => a.startTime - b.startTime);
+  //for each meeting
+  meetings.forEach(meeting => {
+    let included = false;
+    busyTimes.forEach(busy => {
+      //if this start time is before any end-times of meetings in busyTimes
+      if (meeting.startTime <= busy.endTime) {
+        //if this end-time is after the end-time, update the end-time to this end-time
+        if (meeting.endTime > busy.endTime) {
+          busy.endTime = meeting.endTime;
+        }
+        included = true;
+      }
+    });
+    //otherwise, store this meeting in busyTimes
+    !included ? busyTimes.push(meeting) : null;
   });
-  let times = Object.keys(busyTimes).map(Number);
-  let iter = 0;
-  let mergedTimes = [];
-  while (iter < times.length) {
-    let newMeeting = { startTime: times[iter], endTime: null};
-    iter++;
-    while (times[iter] - times[iter - 1] === 1) {
-      iter++;
-    }
-    newMeeting.endTime = times[iter - 1] + 1;
-    mergedTimes.push(newMeeting);
-  }
-  return mergedTimes;
+  return busyTimes;
 }
 
 var input = [
