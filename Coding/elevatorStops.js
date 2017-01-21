d7b679654860e6f4b3fd205c995c8213a7f49de9
Karina Kinaman <kkinaman419@gmail.com>
2017-01-20 21:48:36 -0800

    Add new problem: elevator stops

---
+++ Coding/elevatorStops.js
@@ -0,0 +1,6 @@
+/*
+Given A, an array of people's weights, B, an array of the floors they want to go to, M the number of floors, 
+  X the capacity (#) of people in the elevator, and Y the capacity (weight) of people in the elevator,
+  write a function that outputs how many stops the elevator will make to take all people.
+  Note: The order of people waiting will not change, even if someone from the middle can fit in an elevator earlier
+*/
