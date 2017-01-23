ff75b35c81698abdfd90fdf56daf3f33c262a078
Karina Kinaman <kkinaman419@gmail.com>
2017-01-22 20:33:54 -0800

    Add problem: rectangle overlap

---
+++ Coding/rectOverlap.js
@@ -0,0 +1,20 @@
+/*
+Given two rectangles, as described below, output the rectangle of their overlap.
+
+Format:
+var myRectangle = {
+
+    // coordinates of bottom-left corner
+    leftX: 1,
+    bottomY: 5,
+
+    // width and height
+    width: 10,
+    height: 4,
+
+};
+*/
+
+function findOverlap(rect1, rect2) {
+  
+}
