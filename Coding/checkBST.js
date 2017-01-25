3f9cc4de08e388b9d1e00247551a9fb03cfb3145
Karina Kinaman <kkinaman419@gmail.com>
2017-01-24 21:26:58 -0800

    Add problem: checkbst

---
+++ Coding/checkBST.js
@@ -0,0 +1,23 @@
+/*
+Write a function to check if a BST is valid
+*/
+
+function BinaryTreeNode(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinaryTreeNode.prototype.insertLeft = function(value) {
+  this.left = new BinaryTreeNode(value);
+  return this.left;
+};
+
+BinaryTreeNode.prototype.insertRight = function(value) {
+  this.right = new BinaryTreeNode(value);
+  return this.right;
+};
+
+BinaryTreeNode.prototype.isValid = function() {
+
+};
