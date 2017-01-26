bc1b78cc0baa4a60653bcf2319da76ab3087c1e8
Karina Kinaman <kkinaman419@gmail.com>
2017-01-26 11:17:07 -0800

    Simplify logic

--- Coding/checkBST.js
+++ Coding/checkBST.js
@@ -18,31 +18,24 @@ BinaryTreeNode.prototype.insertRight = function(value) {
   return this.right;
 };
 
-BinaryTreeNode.prototype.isValid = function(bound, direction) {
-  //if a leaf, return true
-  if (!this.left && !this.right) {
-    return true;
-  }
-  let leftValidity = this.left ? this.left.isValid(this.value, 'upper') : true;
-  let rightValidity = this.right ? this.right.isValid(this.value, 'lower') : true;
+BinaryTreeNode.prototype.isValid = function(upperBound, lowerBound) {
+  upperBound = upperBound || Infinity;
+  lowerBound = lowerBound || -Infinity;
+  let leftValidity = this.left ? this.left.isValid(Math.min(upperBound, this.value), lowerBound) : true;
+  let rightValidity = this.right ? this.right.isValid(upperBound, Math.max(lowerBound, this.value)) : true;
   //if this node is valid, return the validity of its left and right nodes
-  if (leftChildValid(this) && rightChildValid(this) && inBound(this.left, this.right, bound, direction)) {
+  if (leftChildValid(this, upperBound, lowerBound) && rightChildValid(this, upperBound, lowerBound)) {
     return leftValidity && rightValidity;
   } else {
     return false;
   }
 
-  function leftChildValid(node) {
-    return !node.left || (node.left && node.left.value < node.value);
+  function leftChildValid(node, upperBound, lowerBound) {
+    return !node.left || (node.left && node.left.value < node.value && node.left.value > lowerBound && node.left.value < upperBound);
   }
 
-  function rightChildValid(node) {
-    return !node.right || (node.right && node.right.value > node.value);
-  }
-
-  function inBound(left, right, bound, direction) {
-    return !bound || (direction === 'upper' && (!left || left.value < bound) && (!right || right.value < bound)) ||
-      (direction === 'lower' && (!left || left.value > bound) && (!right || right.value > bound));
+  function rightChildValid(node, upperBound, lowerBound) {
+    return !node.right || (node.right && node.right.value > node.value && node.right.value > lowerBound && node.right.value < upperBound);
   }
 };
 
@@ -50,7 +43,7 @@ var BST = new BinaryTreeNode(4);
 var node2 = BST.insertLeft(2);
 var node6 = BST.insertRight(6);
 var node1 = node2.insertLeft(1);
-var node3 = node2.insertRight(5); //change for validity; 3 valid, 5 not valid
+var node3 = node2.insertRight(3); //change for validity; 3 valid, 5 not valid
 var node5 = node6.insertLeft(5);
 var node7 = node6.insertRight(7);
 var node8 = node7.insertRight(8);
