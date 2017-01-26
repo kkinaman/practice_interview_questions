cfe9588fe2a4cc0c3b27485694786536637f1e41
Karina Kinaman <kkinaman419@gmail.com>
2017-01-26 10:50:52 -0800

    Handle case where lower children are not valid compared to ancestors

--- Coding/checkBST.js
+++ Coding/checkBST.js
@@ -18,27 +18,37 @@ BinaryTreeNode.prototype.insertRight = function(value) {
   return this.right;
 };
 
-BinaryTreeNode.prototype.isValid = function() {
+BinaryTreeNode.prototype.isValid = function(bound, direction) {
   //if a leaf, return true
   if (!this.left && !this.right) {
     return true;
   }
-  let leftValidity = this.left ? this.left.isValid() : true;
-  let rightValidity = this.right ? this.right.isValid() : true;
+  let leftValidity = this.left ? this.left.isValid(this.value, 'upper') : true;
+  let rightValidity = this.right ? this.right.isValid(this.value, 'lower') : true;
   //if this node is valid, return the validity of its left and right nodes
   if ((!this.left || (this.left && this.left.value < this.value)) &&
     (!this.right || (this.right && this.right.value > this.value))) {
-    return leftValidity && rightValidity;
+    if (inBound(this.left, this.right, bound, direction)) {
+      return leftValidity && rightValidity;
+    } else {
+      return false;
+    }
+    // return leftValidity && rightValidity;
   } else {
     return false;
+  }
+
+  function inBound(left, right, bound, direction) {
+    return !bound || (direction === 'upper' && (!left || left.value < bound) && (!right || right.value < bound)) ||
+      (direction === 'lower' && (!left || left.value > bound) && (!right || right.value > bound));
   }
 };
 
 var BST = new BinaryTreeNode(4);
-var node2 = BST.insertLeft(7);
+var node2 = BST.insertLeft(2);
 var node6 = BST.insertRight(6);
-// var node1 = node2.insertLeft(1);
-// var node3 = node2.insertRight(3);
+var node1 = node2.insertLeft(1);
+var node3 = node2.insertRight(3); //change for validity; 3 valid, 5 not valid
 var node5 = node6.insertLeft(5);
 var node7 = node6.insertRight(7);
 var node8 = node7.insertRight(8);
