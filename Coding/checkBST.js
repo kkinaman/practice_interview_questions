78b9a181a3fc56b71ac870a5ffb074a48795b107
Karina Kinaman <kkinaman419@gmail.com>
2017-01-26 10:54:35 -0800

    Modularize functions

--- Coding/checkBST.js
+++ Coding/checkBST.js
@@ -26,16 +26,18 @@ BinaryTreeNode.prototype.isValid = function(bound, direction) {
   let leftValidity = this.left ? this.left.isValid(this.value, 'upper') : true;
   let rightValidity = this.right ? this.right.isValid(this.value, 'lower') : true;
   //if this node is valid, return the validity of its left and right nodes
-  if ((!this.left || (this.left && this.left.value < this.value)) &&
-    (!this.right || (this.right && this.right.value > this.value))) {
-    if (inBound(this.left, this.right, bound, direction)) {
-      return leftValidity && rightValidity;
-    } else {
-      return false;
-    }
-    // return leftValidity && rightValidity;
+  if (leftChildValid(this) && rightChildValid(this) && inBound(this.left, this.right, bound, direction)) {
+    return leftValidity && rightValidity;
   } else {
     return false;
+  }
+
+  function leftChildValid(node) {
+    return !node.left || (node.left && node.left.value < node.value);
+  }
+
+  function rightChildValid(node) {
+    return !node.right || (node.right && node.right.value > node.value);
   }
 
   function inBound(left, right, bound, direction) {
@@ -48,7 +50,7 @@ var BST = new BinaryTreeNode(4);
 var node2 = BST.insertLeft(2);
 var node6 = BST.insertRight(6);
 var node1 = node2.insertLeft(1);
-var node3 = node2.insertRight(3); //change for validity; 3 valid, 5 not valid
+var node3 = node2.insertRight(5); //change for validity; 3 valid, 5 not valid
 var node5 = node6.insertLeft(5);
 var node7 = node6.insertRight(7);
 var node8 = node7.insertRight(8);
