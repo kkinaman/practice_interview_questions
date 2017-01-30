793080f630ddc7235b2402630fc00888ab239c8d
Karina Kinaman <kkinaman419@gmail.com>
2017-01-30 09:00:02 -0500

    Solve findInSet in log time

--- Coding/findInSet.js
+++ Coding/findInSet.js
@@ -3,8 +3,18 @@ Given an array of integers sorted in ascending order, how quickly can we determi
 */
 
 function hasElement(arr, target) {
-
+  if (arr.length <= 1) {
+    return arr[0] === target || false;
+  }
+  let midpoint = Math.floor(arr.length / 2);
+  if (target === arr[midpoint]) {
+    return true;
+  } else if (target < arr[midpoint]) {
+    return hasElement(arr.slice(0, midpoint), target);
+  } else if (target > arr[midpoint]) {
+    return hasElement(arr.slice(midpoint + 1), target);
+  }
 }
 
 var arr = [1, 2, 3, 4, 5];
-console.log(hasElement(arr, 5));
+console.log(hasElement(arr, 2));
