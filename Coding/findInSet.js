93244421e049415869ef058612a9ddb13b53c013
Karina Kinaman <kkinaman419@gmail.com>
2017-01-30 08:51:22 -0500

    Add new problem findInSet

---
+++ Coding/findInSet.js
@@ -0,0 +1,10 @@
+/*
+Given an array of integers sorted in ascending order, how quickly can we determine if a given integer is in the set?
+*/
+
+function hasElement(arr, target) {
+
+}
+
+var arr = [1, 2, 3, 4, 5];
+console.log(hasElement(arr, 5));
