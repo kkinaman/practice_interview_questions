a827fa37a3a689db8e1dbf64b5e178934c673d87
Karina Kinaman <kkinaman419@gmail.com>
2017-01-30 09:52:42 -0500

    Handle case of no rotation point, now not handling when rotation point is last index

--- Coding/rotationPoint.js
+++ Coding/rotationPoint.js
@@ -6,31 +6,36 @@ Find the index of the 'first' word, or the rotation point
 function findRotationPoint(arr) {
   //find midpoint; split into two subarrays
   let midpoint = Math.floor(arr.length / 2);
+  let startIndex = arguments[1] || 0;
   //compare first and last element of subarray
   //if the first element sorts higher than last element,
-  if (arr[0] > arr[midpoint - 1]) {
+  if (arr[0] > arr[midpoint]) {
     //recurse on that half -- the rotation point is contained in there
-    return findRotationPoint(arr.slice(0, midpoint));
+    return findRotationPoint(arr.slice(0, midpoint + 1), 0);
   } else if (arr[midpoint + 1] > arr[arr.length - 1]) {
-    return findRotationPoint(arr.slice(midpoint));
+    return findRotationPoint(arr.slice(midpoint + 1), midpoint + 1);
   } else {
-    return midpoint;
+    if (arr[midpoint] > arr[midpoint + 1]) {
+      return startIndex + midpoint + 1;
+    } else {
+      return 0;
+    }
   }
 }
 
 var words = [
-  'zigzag',
+  
+  'pig',
+  'ptolemaic',
+  'retrograde',
+  'supplant',
+  'undulate',
+  'xenoepist',
   'asymptote', // <-- rotates here!
   'babka',
   'banoffee',
   'engender',
   'karpatka',
   'othellolagkage',
-  'pig',
-  'ptolemaic',
-  'retrograde',
-  'supplant',
-  'undulate',
-  'xenoepist'
 ];
 console.log(findRotationPoint(words));
