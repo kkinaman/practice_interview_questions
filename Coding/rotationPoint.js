35e39c31dbd742e475164203dd26c9eedbaf6a33
Karina Kinaman <kkinaman419@gmail.com>
2017-01-30 09:10:46 -0500

    Solve except if array has no rotation

--- Coding/rotationPoint.js
+++ Coding/rotationPoint.js
@@ -4,20 +4,33 @@ Find the index of the 'first' word, or the rotation point
 */
 
 function findRotationPoint(arr) {
-
+  //find midpoint; split into two subarrays
+  let midpoint = Math.floor(arr.length / 2);
+  //compare first and last element of subarray
+  //if the first element sorts higher than last element,
+  if (arr[0] > arr[midpoint - 1]) {
+    //recurse on that half -- the rotation point is contained in there
+    return findRotationPoint(arr.slice(0, midpoint));
+  } else if (arr[midpoint + 1] > arr[arr.length - 1]) {
+    return findRotationPoint(arr.slice(midpoint));
+  } else {
+    return midpoint;
+  }
 }
 
 var words = [
-  'ptolemaic',
-  'retrograde',
-  'supplant',
-  'undulate',
-  'xenoepist',
+  'zigzag',
   'asymptote', // <-- rotates here!
   'babka',
   'banoffee',
   'engender',
   'karpatka',
   'othellolagkage',
+  'pig',
+  'ptolemaic',
+  'retrograde',
+  'supplant',
+  'undulate',
+  'xenoepist'
 ];
 console.log(findRotationPoint(words));
