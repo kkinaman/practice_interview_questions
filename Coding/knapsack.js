2c4392e87a12e219be10ad960293f67c77f678c3
Karina Kinaman <kkinaman419@gmail.com>
2017-02-07 21:02:40 -0800

    Handle cases of 0 capacity or 0 weight

--- Coding/knapsack.js
+++ Coding/knapsack.js
@@ -8,18 +8,20 @@ function maxDuffelBagValue(tuples, capacity) {
   function recurse(options, weightSoFar, valSoFar) {
     for (var i = 0; i < options.length; i++) {
       let option = options[i];
-      if (weightSoFar + option[0] < capacity) {
-        recurse(options.slice(i), weightSoFar + option[0], valSoFar + option[1]);
-      } else if (weightSoFar + option[0] === capacity && valSoFar + option[1] > maxValue) {
-        maxValue = valSoFar + option[1];
+      if (option[0] !== 0) {
+        if (weightSoFar + option[0] < capacity) {
+          recurse(options.slice(i), weightSoFar + option[0], valSoFar + option[1]);
+        } else if (weightSoFar + option[0] === capacity && valSoFar + option[1] > maxValue) {
+          maxValue = valSoFar + option[1];
+        }
       }
     }
   }
   recurse(tuples, 0, 0);
-  return maxValue;
+  return maxValue === -Infinity ? 0 : maxValue;
 }
 
-var cakeTuples = [[7, 160], [3, 90], [2, 15]];
+var cakeTuples = [[0, 0], [3, 90], [2, 15]];
 var capacity = 20;
 
 console.log(maxDuffelBagValue(cakeTuples, capacity));
