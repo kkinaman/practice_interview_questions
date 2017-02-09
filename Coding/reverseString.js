034816bddac535d54f06ba26083bd764b8141067
Karina Kinaman <kkinaman419@gmail.com>
2017-02-08 22:08:45 -0800

    Implement reverse string

--- Coding/reverseString.js
+++ Coding/reverseString.js
@@ -1,7 +1,21 @@
 /*
 Given a string, output it reversed
+O(n) time, O(1) space
 */
 
 function reverseString(str) {
-  
+  let chars = str.split('');
+  let mid = Math.floor(chars.length / 2);
+  let iter = 0;
+  while (iter < mid) {
+    let temp = chars[iter];
+    chars[iter] = chars[chars.length - 1 - iter];
+    chars[chars.length - 1 - iter] = temp;
+    iter++;
+  }
+  return chars.join('');
 }
+
+var string = 'hello there ;)';
+
+console.log(reverseString(string));
