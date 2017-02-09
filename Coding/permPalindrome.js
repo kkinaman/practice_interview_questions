77706efb510cd437d7b9ef3fb3daa59af6b2c60a
Karina Kinaman <kkinaman419@gmail.com>
2017-02-08 22:20:49 -0800

    Solve in linear time, arguably constant/linear space
    Space is linear in respect to # of unique letters -- could be argued is constant

--- Coding/permPalindrome.js
+++ Coding/permPalindrome.js
@@ -3,9 +3,19 @@ Check if any permutation of a string is a palindrome
 */
 
 function permutationPalindrome(str) {
-
+  let letterChecker = {};
+  for (var i = 0; i < str.length; i++) {
+    if (letterChecker[str[i]]) {
+      delete letterChecker[str[i]];
+    } else {
+      letterChecker[str[i]] = true;
+    }
+  }
+  return Object.keys(letterChecker).length <= 1;
 }
 
 console.log(permutationPalindrome('civic')); //true
 console.log(permutationPalindrome('ivcci')); //true
 console.log(permutationPalindrome('civil')); //false
+console.log(permutationPalindrome('aabbccddeeffh')); //true
+console.log(permutationPalindrome('aabbccddeeffha')); //false
