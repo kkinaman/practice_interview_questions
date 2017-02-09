5c1b379f72ee1b55ca5fa7e2ac086f57e08c5d22
Karina Kinaman <kkinaman419@gmail.com>
2017-02-08 22:03:58 -0800

    Implement reverse ll

--- Coding/reverseLinkedList.js
+++ Coding/reverseLinkedList.js
@@ -18,8 +18,26 @@ ListNode.prototype.print = function() {
 var a = new ListNode(1);
 var b = new ListNode(2);
 var c = new ListNode(3);
+var d = new ListNode(4);
 
 a.next = b;
 b.next = c;
+c.next = d;
+
+ListNode.prototype.reversed = function() {
+  let cur = this;
+  let leader = cur.next;
+  cur.next = null;
+  let chaser = leader;
+  while (leader) {
+    leader = chaser.next;
+    chaser.next = cur;
+    cur = chaser;
+    chaser = leader;
+  }
+  return cur;
+};
 
 console.log(a.print());
+
+console.log(a.reversed().print());
