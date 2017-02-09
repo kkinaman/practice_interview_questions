ff6003fe8588171b4a161507af549600e647b8aa
Karina Kinaman <kkinaman419@gmail.com>
2017-02-08 22:15:45 -0800

    Add problem: permutaiton palindrome

---
+++ Coding/permPalindrome.js
@@ -0,0 +1,11 @@
+/*
+Check if any permutation of a string is a palindrome
+*/
+
+function permutationPalindrome(str) {
+
+}
+
+console.log(permutationPalindrome('civic')); //true
+console.log(permutationPalindrome('ivcci')); //true
+console.log(permutationPalindrome('civil')); //false
