18c2294e066cf66af72e5875d82c329df0b4bed6
Karina Kinaman <kkinaman419@gmail.com>
2017-02-08 21:15:23 -0800

    Solve kth linked list

--- Coding/kthLinkedList.js
+++ Coding/kthLinkedList.js
@@ -1,22 +1,40 @@
 /*
-Write a function kth_to_last_node() that takes an integer k and the head_node of a singly linked list, 
+Write a function kthToLastNode() that takes an integer k and the head_node of a singly linked list, 
 and returns the kth to last node in the list.
 
 */
 function LinkedListNode(value) {
   this.value = value;
-  this.next = None;
+  this.next = null;
 }
 
-var a = LinkedListNode('Angel Food');
-var b = LinkedListNode('Bundt');
-var c = LinkedListNode('Cheese');
-var d = LinkedListNode('Devil\'s Food');
-var e = LinkedListNode('Eccles');
+var a = new LinkedListNode('Angel Food');
+var b = new LinkedListNode('Bundt');
+var c = new LinkedListNode('Cheese');
+var d = new LinkedListNode('Devil\'s Food');
+var e = new LinkedListNode('Eccles');
 
 a.next = b;
 b.next = c;
 c.next = d;
 d.next = e;
 
-console.log(kth_to_last_node(2, a)); // d
+function kthToLastNode(k, head) {
+  //iterate through the list
+  //once we get to the kth node, start another iterator
+  //when we get to the end, the second iterator will be the kth to last
+  let iter = head;
+  let kth;
+  let counter = 0;
+  while (iter) {
+    counter++;
+    if (counter === k) {
+      kth = head;
+    }
+    kth = kth !== undefined && iter.next !== null ? kth.next : kth;
+    iter = iter.next;
+  }
+  return kth.value;
+}
+
+console.log(kthToLastNode(5, a)); // d
